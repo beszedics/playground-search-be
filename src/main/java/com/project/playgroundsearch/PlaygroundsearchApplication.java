fff9931f5656c5d1067de8ce4d15fa757cef6479
Tamas Beszedics <beszedicshun@gmail.com>
2021-09-17 16:59:24 +0200

    Initial commit

---
+++ src/main/java/com/project/playgroundsearch/PlaygroundsearchApplication.java
@@ -0,0 +1,13 @@
+package com.project.playgroundsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundsearchApplication.class, args);
+	}
+
+}

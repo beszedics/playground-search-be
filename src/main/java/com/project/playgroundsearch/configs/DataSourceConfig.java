e6da525d2760919a34cf9a2ab11fcd5400c9b098
Tamas Beszedics <beszedicshun@gmail.com>
2021-09-17 17:31:38 +0200

    Add DataSource config

---
+++ src/main/java/com/project/playgroundsearch/configs/DataSourceConfig.java
@@ -0,0 +1,21 @@
+package com.project.playgroundsearch.configs;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName("com.mysql.cj.jdbc.Driver")
+                .url("jdbc:mysql://localhost:3306/playgroundsearchdb")
+                .username("root")
+                .password("root1234")
+                .build();
+    }
+}
